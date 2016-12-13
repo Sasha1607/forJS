6112ce66bb4659083d64d7313d93980b61ff042c
Sasha Atl <sasha1998f@mail.ru>
2016-12-13 16:01:50 +0000

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+//for (var i = 10; i >= 0; i--){
+   // if (i % 2 === 0) {
+    //document.write(i);
+    //document.write('<br>');
+    //} 
+//}
+/*var i=0;
+while (i<=10) {
+    document.write(i);
+    document.write('<br>');
+    i++;
+}
+*/
+
+/*var i=0;
+do{
+ 
+    document.write(i);
+    document.write('<br>');
+    i++;
+} while (i<=10)
+*/
+var arr = [1,2,3];
+arr[10]= 10;
+
+for (var = i; i = 0; i < arr.length; i++;) {
+
+    
+}
